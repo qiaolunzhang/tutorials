e9dfb7c8e5b3ca7b4f1264648c18201aff65b819
Qiaolun Zhang <qiaolunzhang@gmail.com>
2025-06-11 09:08:14 +0200

    add comments to create docker file

--- micro2024/create_docker_container.sh
+++ micro2024/create_docker_container.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# docker run: Runs a container from a specified image.
+# -i Interactive: Keeps STDIN open so you can interact with the container (e.g., use a shell).
+# -t Terminal: Allocates a pseudo-terminal to make interaction easier.
+# -v Volume mount: Maps the current host directory ($(pwd)) to /app/micro2024 inside the container.
 docker run -i -t \
   --name micro2024-container \
   -v $(pwd):/app/micro2024 \
